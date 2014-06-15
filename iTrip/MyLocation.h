b179eba9545fe4713cce6c689dfb31bec5fedae4
happiness9721 <t9590345@gmail.com>
2014-06-15 21:23:29 +0800

    fix warning

--- iTrip/MyLocation.h
+++ iTrip/MyLocation.h
@@ -17,8 +17,8 @@
 }
 
 @property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
-@property (nonatomic, retain) NSString *title;
-@property (nonatomic, retain) NSString *subtitle;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *subtitle;
 
 -(id) initWithCoordinate: (CLLocationCoordinate2D) the_coordinate;
 @end
